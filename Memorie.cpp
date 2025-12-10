78c659aa906c9d90a33b5d77a1cae74508cd64e2
Alex1221221 <alexandrucoman53@gmail.com>
2025-12-10 22:46:24 +0200

    Create Memorie.cpp

---
+++ Memorie.cpp
@@ -0,0 +1,12 @@
+struct Exemplu
+{
+  short int b   // 2 bytes
+  long long int c // 8 bytes
+  char d // 1 byte
+  double e // 8 bytes
+  int f // 4 bytes
+  bool g // 1 byte
+  int* note // pe un sistem de 64 de biti pointerii ocupa 8 bytes
+  double* p // 4 sau 8 bytes
+  char[5] // 1 * 5 = 5 bytes
+}
