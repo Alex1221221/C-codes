50b2c038de9ef999e03deb73288695ebb101e7d1
Alex1221221 <alexandrucoman53@gmail.com>
2026-02-09 16:34:59 +0200

    Add files via upload

---
+++ exercitiiusoare.cpp
@@ -0,0 +1,94 @@
+//1.suma a doua numere
+
+/*#include <iostream>
+using namespace std;
+
+
+int main(){
+    int a,b;
+    cout<<"Introduceti primul numar: ";
+    cin>>a;
+    cout<<"Introduceti al doilea numar: ";
+    cin>>b;
+
+    int suma=a+b;
+
+    cout<<"Suma numerelor a si b este: "<<suma<<endl;
+
+}
+    */
+
+//2.Par sau Impar
+
+/*#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Introduceti un numar: ";
+    cin>>a;
+    if(a%2==0){
+        cout<<"Numarul "<<a<<" este par."<<endl;}
+
+    else{
+        cout<<"Numarul "<<a<<" este impar."<<endl;
+    }
+    }
+    */
+
+    //3.Maximul dintre 3 numere:
+
+
+    /*
+    #include <iostream>
+    using namespace std;
+
+    int main(){
+        int a,b,c;
+        cout<<"Introduceti primul numar: ";
+        cin>>a;
+        cout<<"Introduceti al doilea numar: ";
+        cin>>b;
+        cout<<"Introduceti al treilea numar: ";
+        cin>>c;
+        
+        if(a>=b && a>=c){
+            cout<<"Numarul "<<a<<" este numarul maxim."<<endl;
+
+        }
+        else if(b>=a && b>=c){
+            cout<<"Numarul "<<b<<" este numarul maxim."<<endl;
+
+        }
+        else{
+            cout<<"Numarul "<<c<<" este numarul maxim."<<endl;
+        }
+        return 0;
+    }
+        */
+
+    //4.Numere de la 1 la n
+
+   /*
+   #include <iostream>
+    using namespace std;
+
+int main() {
+    int n;
+    cout << "Introduceti un numar: ";
+    cin >> n;
+
+    if (n < 1) {
+        cout << "Numarul trebuie sa fie mai mare sau egal cu 1." << endl;
+        return 0;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
+*/
+
+
