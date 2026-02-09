89ca1a4bd7982b5a54be4f03a2dd2ff13c11db12
Alex1221221 <alexandrucoman53@gmail.com>
2026-02-09 16:33:04 +0200

    Add files via upload

---
+++ cuvinte.cpp
@@ -0,0 +1,42 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    srand(time(NULL));
+    int secret=rand()%100+1;
+    int guess;
+    cout<<"Ghiceste numarul secret intre 1 si 100: ";
+    cin>>guess;
+    int tries=1;
+    
+
+    while(guess!=secret){
+        if(guess<secret){
+            cout<<"numarul secret este mai mare . Incearca din nou: ";
+            cin>>guess;
+            tries++;
+        }
+        else{
+            cout<<"numarul secret este mai mic . Incearca din nou: ";
+            cin>>guess;
+            tries++;
+        }
+
+}
+    if(tries<5){
+        cout<<"esti bun!"<<endl;
+
+    }
+    else if(tries<10){
+        cout<<"foarte bineeee"<<endl;
+
+    }
+    else{
+        cout<<"bravo!"<<endl;
+    }
+    cout<<"ai ghicit in "<<tries<<" incercari,felicitari!"<<endl;
+}
